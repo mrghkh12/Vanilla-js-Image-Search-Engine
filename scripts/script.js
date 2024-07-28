0ed8fa2d525c34d366dc9507b0f61c07103c9516
mohammad reza <mrghkh12@gmail.com>
2024-07-28 14:39:09 +0330

    feat(script.js): generate img element & del img template element

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,35 @@
 const $ = document
 
+const imageContainer = $.querySelector('.image-container')
+
 const apiKey = '1JFcrBeutxzbXKbDXJacTLeidhb6FDb51Bi4I5Lw6D7NTgTj8p7kIxyR'
 
 const perPage = 15;
 let currentPage = 1;
 
+
+const generateImgElement = (imgData) => {
+    imgData.forEach(imgItem => {
+        imageContainer.insertAdjacentHTML('beforeend' , 
+            `<li class="img-card">
+                <img src="${imgItem.src.large2x}">
+
+                <div class="details">
+                    <div class="photoGrapher">
+                        <i class="uil uil-camera"></i>
+                        <span>${imgItem.photographer}</span>
+                    </div>
+                    <button class="dl-btn"><i class="uil uil-import"></i></button>
+                </div>
+            </li>`
+        )
+    })
+}
+
 async function getImages(apiURL) {
-    let responce = await fetch(apiURL, { headers: { Authorization: apiKey } });
-    let data = await responce.json();
-    console.log(data);
+    let response = await fetch(apiURL, { headers: { Authorization: apiKey } });
+    let data = await response.json();
+    generateImgElement(data.photos);
 }
 
 
